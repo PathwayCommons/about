2893be79d847f7d8071364c25b3c39295602c82a
B. Arman Aksoy <arman@aksoy.org>
2013-08-05 17:45:32 -0400

    redesigned the page. Having a strange issue with the top-navbar.

--- src/main/webapp/js/pc.js
+++ src/main/webapp/js/pc.js
@@ -1,15 +1,4 @@
 $(function(){ // document ready
-    if (!!$('.sticky').offset()) { // make sure ".sticky" element exists
-        var stickyTop = $('.sticky').offset().top; // returns number
-        $(window).scroll(function(){ // scroll event
-            var windowTop = $(window).scrollTop(); // returns number
-            if (stickyTop < windowTop) {
-                $('.sticky').css({ position: 'fixed', top: 5 });
-            } else {
-                $('.sticky').css('position','static');
-            }
-        });
-    }
 
     $("#explore-form").submit(function() {
         window.location

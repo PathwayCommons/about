6f366ea2ee6563a378cea5de2e972fc24e8afd23
B. Arman Aksoy <arman@aksoy.org>
2013-08-20 16:48:57 -0400

    added google analytics and also fixed a bug within the PCViz URL.

--- src/main/webapp/js/pc.js
+++ src/main/webapp/js/pc.js
@@ -2,7 +2,7 @@ $(function(){ // document ready
 
     $("#explore-form").submit(function() {
         window.location
-            = "http://awabi.cbio.mskcc.org/cpath2-demo/#neighborhood/" + $("#gene-text").val().replace(/ /g, "");
+            = "http://www.pathwaycommons.org/pcviz/#neighborhood/" + $("#gene-text").val().replace(/ /g, "");
         return false;
     });
 

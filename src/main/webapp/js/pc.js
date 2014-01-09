2f049ce6f14c7620842274d43fe840b6287d8c8a
B. Arman Aksoy <arman@aksoy.org>
2014-01-09 07:49:35 -0500

    small changes to start tracking the 'explore' button click events.

---
+++ src/main/webapp/js/pc.js
@@ -0,0 +1,41 @@
+$(function(){ // document ready
+
+    $("#explore-form").submit(function() {
+        window.location
+            = "http://www.pathwaycommons.org/pcviz/#neighborhood/" + $("#gene-text").val().replace(/ /g, "");
+        return false;
+    });
+
+    $("a.smooth-scroll").click(function(e) {
+        e.preventDefault();
+
+        var whereTo = $($(this).attr("href")).offset().top - 5;
+
+        $('html, body').animate({
+            scrollTop: whereTo
+        }, 500);
+    });
+
+    $("a.top-scroll").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 300);
+    });
+
+    $(".tile .btn").each(function() {
+        var button = this;
+        var btnLink = $(this).attr("href");
+
+        $(button).parent().click(function(e) {
+            e.preventDefault();
+            window.open(btnLink, "_blank");
+        });
+    });
+
+    // Track outbound links in Google Analytics
+    $(function() {
+        $("a, button").on('click',function(e){
+            var url = $(this).attr("href");
+            ga('send', 'event', 'link', window.location.href, url);
+        });
+    });
+});

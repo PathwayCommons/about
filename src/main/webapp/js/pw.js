8cdb474fb7ca4cda687c31d4d52e3f8574d93121
rodche <rodche@gmail.com>
2014-04-25 16:30:38 -0400

    Added dependencies, routs, etc. (TODO: add /top_pathways and /find_pathways json web services, or use/setup jsonp queries..; try angular partials, i.e. inserts instead of states and switch logic...; could implement these pages in the cpath2 project instead of this one)

--- src/main/webapp/js/pw.js
+++ src/main/webapp/js/pw.js
@@ -1,63 +1,89 @@
+"use strict";
+
 $(function(){ // document ready
 
     $("#find-form").submit(function() {
     	var keyword = $("#keyword-text").val();
     	
     	if(keyword)
-    		window.location = "pathways.html?find=" + encodeURIComponent(keyword);
+    		window.location = "#/pw/" + encodeURIComponent(keyword);
     	
         return false;
     });
-
-    $("a.smooth-scroll").click(function(e) {
-        e.preventDefault();
-
-        var whereTo = $($(this).attr("href")).offset().top - 5;
-
-        $('html, body').animate({
-            scrollTop: whereTo
-        }, 500);
-    });
-
-    $("a.top-scroll").click(function(e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: 0 }, 300);
-    });
-
-    $(".tile .btn").each(function() {
-        var button = this;
-        var btnLink = $(this).attr("href");
-
-        $(button).parent().click(function(e) {
-            e.preventDefault();
-            window.open(btnLink, "_blank");
-        });
-    });
-
-    // Track outbound links in Google Analytics
-    $(function() {
-        $("a, button").on('click',function(e){
-            var url = $(this).attr("href");
-            ga('send', 'event', 'link', window.location.href, url);
-        });
-    });
     
-    
-    // Query PC2 web service - search for pathways
-    $(function() {
-    	var keyword = urlParam('find');
-    	$("#keyword-text").val(keyword);
-    	
-    	//TODO get top_pathways or search for pathways, and update '#pathways-list'
-    	
-    	return false;
-    });
-
 });
 
-function urlParam(name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+
+// AngularJS app and controllers:
+
+var pcApp = angular.module('pcApp', ['ngRoute']);
+
+// configure routes (client-side)
+pcApp.config(function( $routeProvider ) 
+{
+	$routeProvider
+		.when(
+			"/home",
+			{ action: "home.default" }
+		)
+		.when(
+			"/pw",
+			{ action: "pw.default" }
+		)
+		.when(
+			"/pw/:kw",
+			{ action: "pw.find" }
+		)
+		.otherwise(
+			{ redirectTo: "/home" }
+		);
+});
+
+pcApp.controller('PcController', function($scope, $route, $routeParams, $http) {
+	
+	var render = function() {
+		
+		var renderAction = $route.current.action;
+		if(!renderAction)
+			return;	
+		
+		$scope.renderAction = renderAction;	
+		
+		var renderPath = renderAction.split( "." );
+		$scope.renderPath = renderPath;
+		
+		if(renderPath[0] == "pw") {
+
+			//get the query parameter, if any
+			var kw = ($routeParams.kw || "");
+			$("#keyword-text").val(kw);
+
+			if(kw) {
+				//find pathways by keyword(s)
+				$http.get('/find_pathway/' + 
+						encodeURIComponent(kw)).success(function(data) 
+				{
+					$scope.hits = data;
+				});	
+			} else {
+				//list top pathways (callback=JSON_CALLBACK is very important!)
+				$http.get('/top_pathways').success(function(data) {
+					$scope.hits = data;
+				});	
+			}
+		}
+		
+	};
+    
+    // Listen for changes to the Route. When the route
+    // changes, update view.
+    $scope.$on(
+        "$routeChangeSuccess",
+        function( $currentRoute, $previousRoute ){
+            // Update the rendering.
+            render();
+        }
+    );
+	
+});
+
